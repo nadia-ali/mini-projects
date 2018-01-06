302332fe2b0c79626ef6d685ef415af3654749f1
nagnes1995 <nadiaali95@live.co.uk>
2018-01-06 13:48:27 +0000

    tests

--- box/test_draw_box.py
+++ box/test_draw_box.py
@@ -1,20 +1,16 @@
 import pytest
 from draw_box import draw_box
 
-""" These tests are checking the length of the height and width of the boxes """
+""" These tests are checking the output of the draw_box function given the height and width """
 
 def test_4x4():
-	""" This function is testing the input height 4 is now a height of 2 """
 	assert draw_box(4,4) == '┌──┐\n│  │\n└──┘'
 
 def test_6x6():
-	""" This function is testing the input width 8 is now a width of 4 """
 	assert draw_box(6,6) == '┌───┐\n│   │\n└───┘'
 
 def test_8x8():
-	""" This function is testing the input height 4 is now a height of 2 """
 	assert draw_box(8,8) == '┌────┐\n│    │\n└────┘'
 
 def test_10x10():
-	""" This function is testing the input height 4 is now a height of 2 """
 	assert draw_box(10,10) == '┌─────┐\n│     │\n└─────┘'
